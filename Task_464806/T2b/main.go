3d389d0f785e1c68c1864f44118ae4011bef1869
admin <admin@Macbooks-MacBook-Pro.local>
2024-12-21 15:03:40 +0530

    task_464885

--- Task_464806/T2b/main.go
+++ Task_464806/T2b/main.go
@@ -2,17 +2,17 @@ package main
 
 import "fmt"
 
-// func main() {
-// 	nums := []int{1, 2, 3, 4, 5, 6, 7}
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
 
-// 	// Incorrect way: Deleting elements during iteration
-// 	for i, num := range nums {
-// 		if num%2 == 0 {
-// 			nums = append(nums[:i], nums[i+1:]...)
-// 		}
-// 	}
-// 	fmt.Println(nums) // Output: [1 3 5 7]
-// }
+	// Incorrect way: Deleting elements during iteration
+	for i, num := range nums {
+		if num%2 == 0 {
+			nums = append(nums[:i], nums[i+1:]...)
+		}
+	}
+	fmt.Println(nums) // Output: [1 3 5 7]
+}
 
 // package main
 
@@ -32,11 +32,11 @@ import "fmt"
 
 // import "fmt"
 
-func main() {
-	myMap := map[string]int{"a": 1, "b": 2, "c": 3}
+// func main() {
+// 	myMap := map[string]int{"a": 1, "b": 2, "c": 3}
 
-	// Correct way: Iterating over keys
-	for key := range myMap {
-		fmt.Println("Key:", key)
-	}
-}
+// 	// Correct way: Iterating over keys
+// 	for key := range myMap {
+// 		fmt.Println("Key:", key)
+// 	}
+// }
