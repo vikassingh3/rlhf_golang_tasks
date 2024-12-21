de9204fc99946f1427aa308d4bf746d7ccbd8bee
admin <admin@Macbooks-MacBook-Pro.local>
2024-12-21 13:55:07 +0530

    modified

--- Task_464806/T2a/main.go
+++ Task_464806/T2a/main.go
@@ -1,27 +1,61 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 func removeDuplicates(m map[int]int) {
-    keys := make([]int, 0, len(m))
-    for key := range m {
-        keys = append(keys, key)
-    }
+	keys := make([]int, 0, len(m))
+	for key := range m {
+		keys = append(keys, key)
+	}
 
-    for _, key := range keys {
-        value := m[key]
-        if value > 1 {
-            delete(m, key)
-        }
-    }
+	for _, key := range keys {
+		value := m[key]
+		if value > 1 {
+			delete(m, key)
+		}
+	}
 }
 
 func main() {
-    data := map[int]int{1: 1, 2: 2, 3: 3, 4: 3, 5: 4}
+	data := map[int]int{1: 1, 2: 2, 3: 3, 4: 3, 5: 4}
 
-    removeDuplicates(data)
+	removeDuplicates(data)
 
-    fmt.Println(data)
+	fmt.Println(data)
+
+	data2 := map[int]int{1: 1, 2: 2, 3: 3, 4: 3, 5: 4}
+
+	removeDuplicates1(data2)
+
+	fmt.Println(data2)
 }
+
+// package main
+
+// import (
+//     "fmt"
+// )
+
+func removeDuplicates1(m map[int]int) {
+	keys := make([]int, 0, len(m))
+	for key := range m {
+		keys = append(keys, key)
+	}
+
+	for _, key := range keys {
+		value := m[key]
+		if value > 1 {
+			delete(m, key)
+		}
+	}
+}
+
+// func main() {
+// 	data := map[int]int{1: 1, 2: 2, 3: 3, 4: 3, 5: 4}
+
+// 	removeDuplicates(data)
+
+// 	fmt.Println(data)
+// }
--- Task_464806/T2b/main.go
+++ Task_464806/T2b/main.go
@@ -2,41 +2,41 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	nums := []int{1, 2, 3, 4, 5, 6, 7}
+
+// 	// Incorrect way: Deleting elements during iteration
+// 	for i, num := range nums {
+// 		if num%2 == 0 {
+// 			nums = append(nums[:i], nums[i+1:]...)
+// 		}
+// 	}
+// 	fmt.Println(nums) // Output: [1 3 5 7]
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	myMap := map[string]int{"a": 1, "b": 2, "c": 3}
+
+// 	// Incorrect way: Iterating over values using delete during iteration
+// 	for _, value := range myMap {
+// 		delete(myMap, "b")
+// 		fmt.Println("Value:", value)
+// 	}
+// }
+
+// package main
+
+// import "fmt"
+
 func main() {
-	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	myMap := map[string]int{"a": 1, "b": 2, "c": 3}
 
-	// Incorrect way: Deleting elements during iteration
-	for i, num := range nums {
-		if num%2 == 0 {
-			nums = append(nums[:i], nums[i+1:]...)
-		}
+	// Correct way: Iterating over keys
+	for key := range myMap {
+		fmt.Println("Key:", key)
 	}
-	fmt.Println(nums) // Output: [1 3 5 7]
 }
-
-// package main
-
-// import "fmt"
-
-// func main() {
-//     myMap := map[string]int{"a": 1, "b": 2, "c": 3}
-
-//     // Incorrect way: Iterating over values using delete during iteration
-//     for _, value := range myMap {
-//         delete(myMap, "b")
-//         fmt.Println("Value:", value)
-//     }
-// }
-
-// package main
-
-// import "fmt"
-
-// func main() {
-//     myMap := map[string]int{"a": 1, "b": 2, "c": 3}
-
-//     // Correct way: Iterating over keys
-//     for key := range myMap {
-//         fmt.Println("Key:", key)
-//     }
-// }
