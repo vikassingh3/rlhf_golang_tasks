209d00279a864affaa7fec8bd2a2ea476649ac7c
Vikas Kumar <vikas.k.quytech@aux.gojek.com>
2024-12-28 19:16:45 +0530

    task

---
+++ NewTask/Task_493989/T1b/main.go
@@ -0,0 +1,16 @@
+package main  
+import (  
+    "fmt"
+    "time"
+)  
+func main() {  
+    m := make(map[int]int)  
+    start := time.Now()  
+    // Add elements from 0 to a large number (e.g., 10 million)
+    const numElements = 10000000
+    for i := 0; i < numElements; i++ {  
+        m[i] = i  
+    }  
+    elapsed := time.Since(start)  
+    fmt.Printf("Time taken to add %d elements to the map: %s\n", numElements, elapsed)  
+}  
