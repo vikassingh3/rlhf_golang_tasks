d043034d5a6cb4ec788fce7091ed1374bb8c8abd
Vikas Kumar <vikas.k.quytech@aux.gojek.com>
2025-01-01 16:39:19 +0530

    task

--- NewTask/Task_493989/T1b/main.go
+++ NewTask/Task_493989/T1b/main.go
@@ -1,16 +1,28 @@
-package main  
-import (  
-    "fmt"
-    "time"
-)  
-func main() {  
-    m := make(map[int]int)  
-    start := time.Now()  
-    // Add elements from 0 to a large number (e.g., 10 million)
-    const numElements = 10000000
-    for i := 0; i < numElements; i++ {  
-        m[i] = i  
-    }  
-    elapsed := time.Since(start)  
-    fmt.Printf("Time taken to add %d elements to the map: %s\n", numElements, elapsed)  
-}  
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	m := make(map[int]int)
+	start := time.Now()
+	// Add elements from 0 to a large number (e.g., 10 million)
+	const numElements = 10000000
+	for i := 0; i < numElements; i++ {
+		m[i] = i
+	}
+	elapsed := time.Since(start)
+	fmt.Printf("Time taken to add %d elements to the map (with resizing)   : %s\n", numElements, elapsed)
+
+        start = time.Now()
+	mm := make(map[int]int, 10000000)
+	// Add elements from 0 to a large number (e.g., 10 million)
+	for i := 0; i < numElements; i++ {
+		mm[i] = i
+	}
+	elapsed = time.Since(start)
+	fmt.Printf("Time taken to add %d elements to the map (without resizing): %s\n", numElements, elapsed)
+
+}
